39822821744c2f0f4a78dc5ef464c58c397a49d0
Ben Booth <ben@benbooth.dev>
2021-03-22 12:43:35 +1100

    Add initial definitions for icon-sdk-js

--- index.d.ts
+++ index.d.ts
@@ -3,37 +3,213 @@
 // Definitions by: Ben Booth <https://github.com/bkbooth>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
-/*~ If this module is a UMD module that exposes a global variable 'myLib' when
- *~ loaded outside a module loader environment, declare that global here.
- *~ Otherwise, delete this declaration.
- */
-export as namespace myLib;
+import { BigNumber } from "bignumber.js";
 
-/*~ If this module has methods, declare them as functions like so.
- */
-export function myMethod(a: string): string;
-export function myOtherMethod(a: number): number;
+type Key = string | Buffer | ArrayBuffer | Array<any>;
+type BigNumberParseable = string | number | BigNumber;
 
-/*~ You can declare types that are available via importing the module */
-export interface someType {
-    name: string;
-    length: number;
-    extras?: string[];
+interface KeyValue {
+    [key: string]: any;
 }
 
-/*~ You can declare properties of the module using const, let, or var */
-export const myField: number;
-
-/*~ If there are types, properties, or methods inside dotted names
- *~ of the module, declare them inside a 'namespace'.
- */
-export namespace subProp {
-    /*~ For example, given this definition, someone could write:
-     *~   import { subProp } from 'yourModule';
-     *~   subProp.foo();
-     *~ or
-     *~   import * as yourMod from 'yourModule';
-     *~   yourMod.subProp.foo();
+export default class IconService {
+    /**
+     * Creates an instance of IconService.
      */
-    function foo(): void;
+    constructor(provider: HttpProvider);
+
+    /**
+     * Get the balance of the address.
+     */
+    getBalance(address: string): HttpCall;
+
+    /**
+     * Calls a SCORE API just for reading.
+     */
+    call(call: Call): HttpCall;
+}
+
+export class IconAmount {
+    public value: BigNumber;
+    public digit: number;
+
+    /**
+     * Creates an instance of IconAmount.
+     *
+     * Note: According to official document of BigNumber.js,
+     * it is recommended to create BigNumbers from String values rather than Number values
+     * to avoid a potential loss of precision.
+     */
+    constructor(value: BigNumberParseable, digit: BigNumberParseable);
+
+    /**
+     * Creates an instance of IconAmount.
+     */
+    static of(value: BigNumberParseable, digit: BigNumberParseable): IconAmount;
+
+    /**
+     * Get digit property.
+     */
+    getDigit(): number;
+
+    /**
+     * Convert value property into string
+     */
+    toString(): string;
+
+    /**
+     * Convert the unit of value property into loop
+     */
+    toLoop(): BigNumber;
+
+    /**
+     * Convert the unit of value property into custom digit
+     */
+    convertUnit(digit: BigNumberParseable): IconAmount;
+
+    /**
+     * IconAmount class property which contains unit digit constants
+     */
+    static Unit: { LOOP: 0; GLOOP: 9; ICX: 18 };
+}
+
+declare class Call {
+    constructor(to: string, from: string, data: KeyValue);
+}
+
+export namespace IconBuilder {
+    class CallBuilder {
+        /**
+         * Creates an instance of CallBuilder.
+         */
+        constructor();
+
+        /**
+         * Set 'to' property
+         */
+        to(to: string): this;
+
+        /**
+         * Set 'from' property
+         */
+        from(from: string): this;
+
+        /**
+         * Set 'method' property
+         */
+        method(method: string): this;
+
+        /**
+         * Set 'params' property
+         */
+        params(params: KeyValue): this;
+
+        /**
+         * Build 'Call' object
+         */
+        build(): Call;
+    }
+}
+
+export namespace IconConverter {
+    /**
+     * Convert string or BigNumber value to number.
+     */
+    function toNumber(value: BigNumberParseable): number;
+
+    /**
+     * Convert string or number value to BigNumber.
+     */
+    function toBigNumber(value: BigNumberParseable): BigNumber;
+
+    /**
+     * Convert string, number or BigNumber value to hex string strictly.
+     */
+    function toHexNumber(value: BigNumberParseable): string;
+
+    /**
+     * Convert string, number or BigNumber value to hex string.
+     */
+    function toHex(value: BigNumberParseable): string;
+}
+
+export class IconWallet {
+    /**
+     * Creates an instance of Wallet.
+     */
+    constructor(privateKey: Key, publicKey?: null | undefined);
+    constructor(privateKey: null | undefined, publicKey: Key);
+
+    /**
+     * Creates an instance of Wallet.
+     */
+    static create(): IconWallet;
+
+    /**
+     * Import existing wallet instance using private key.
+     */
+    static loadPrivateKey(privateKey: Key): IconWallet;
+
+    /**
+     * Import existing wallet instance using keystore object.
+     */
+    static loadKeystore(keystore: object | string, password: string, nonStrict?: boolean): IconWallet;
+
+    /**
+     * Get private key of wallet instance.
+     */
+    getPrivateKey(): string;
+
+    /**
+     * Get public key of wallet instance.
+     */
+
+    getPublicKey(): string;
+
+    /**
+     * Get EOA address of wallet instance.
+     */
+    getAddress(): string;
+}
+
+export namespace IconValidator {
+    /**
+     * Check if input value is a private key.
+     */
+    function isPrivateKey(value: Key): boolean;
+
+    /**
+     * Check if input value is a public key.
+     */
+    function isPublicKey(value: Key): boolean;
+
+    /**
+     * Check if input value is a EOA address.
+     */
+    function isEoaAddress(value: any): boolean;
+
+    /**
+     * Check if input value is a SCORE address.
+     */
+    function isScoreAddress(value: any): boolean;
+
+    /**
+     * Check if input value is a EOA or SCORE address.
+     */
+    function isAddress(value: any): boolean;
+}
+
+export class HttpCall {
+    // TODO
+    execute(): Promise<any>;
+}
+
+export class HttpProvider {
+    /**
+     * Creates an instance of HttpProvider.
+     */
+    constructor(url: string);
+
+    // TODO
+    request(value: KeyValue, converter: any): HttpCall;
 }
